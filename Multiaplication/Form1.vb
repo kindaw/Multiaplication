296f4b21de1fa73ca9d63e5ad7bce410817e279d
Ratch <Ratch@CONDITATION>
2024-07-15 10:03:31 +0700

    Add project files.

---
+++ Multiaplication/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Dim setter, multi, resalt As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i As Integer = 1 To 12
+            setter = 15
+            multi = i + 5
+            resalt = setter * multi
+            MessageBox.Show(setter & "X" & multi & "=" & resalt)
+        Next
+    End Sub
+End Class
